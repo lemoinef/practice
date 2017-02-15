48ef6c965ade60e4522edb8baebf513f2312646b
Francois Lemoine <lemoinef@outlook.com>
2017-02-15 18:40:17 -0500

    add check_profanity function to script

--- web_dev/check_profanity.py
+++ web_dev/check_profanity.py
@@ -1,8 +1,22 @@
-import os
+import urllib
 
 def read_text():
 	quotes = open('/home/francois/practice/web_dev/movie_quotes.txt')
 	contents_of_files = quotes.read()
-	print(contents_of_files)
+	# print(contents_of_files)
 	quotes.close()
+	check_profanity(contents_of_files)
+
+def check_profanity(text_to_check):
+	connection = urllib.urlopen('http://www.wdylike.appspot.com/?q='+text_to_check)
+	output = connection.read()
+	# print(output)
+	connection.close()
+	if "true" in output:
+		print('Profanity Alert!')
+	elif "false" in output:
+		print("This document has no curse words!")
+	else:
+		print("Could not scan the document properly")
+
 read_text()
